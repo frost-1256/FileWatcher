0fe6d2b28e9a2a4277b9a0080cae28c02ffd35c9
kabotilya <kabotilya@DESKTOP-LAMP56J>
2024-02-03 20:25:28 +0900

    プロジェクト ファイルを追加します。

---
+++ FileWatcher-v2-Test/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FileWatcher_v2_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(21, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "監視を開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(117, 12);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 1;
+            this.btnEnd.Text = "監視を停止";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "FileSelect";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FileWatcher-v2-Test/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace FileWatcher_v2_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 監視を停止する
+            fileSystemWatcher1.IncludeSubdirectories = false;
+
+            // ファイル変更、作成、削除のイベントをファイル変更メソッドにあげる
+            fileSystemWatcher1.Changed += new FileSystemEventHandler(fileChanged);
+            fileSystemWatcher1.Created += new FileSystemEventHandler(fileChanged);
+            fileSystemWatcher1.Deleted += new FileSystemEventHandler(fileChanged);
+
+            // ファイル名変更のイベントをファイル名変更メソッドにあげる
+            fileSystemWatcher1.Renamed += new RenamedEventHandler(fileRenamed);
+
+            // 監視対象ディレクトリを指定する
+            fileSystemWatcher1.Path = @"C:\testdir";
+
+            // 監視対象の拡張子を指定する（全てを指定する場合は空にする）
+            fileSystemWatcher1.Filter = "*";
+
+            // サブディレクトリ配下も含めるか指定する
+            fileSystemWatcher1.IncludeSubdirectories = true;
+
+            // 監視する変更を指定する
+            fileSystemWatcher1.NotifyFilter = NotifyFilters.LastAccess |
+                               NotifyFilters.LastWrite |
+                               NotifyFilters.FileName |
+                               NotifyFilters.DirectoryName;
+
+            // 通知を格納する内部バッファ 既定値は 8192 (8 KB)  4 KB ～ 64 KB
+            fileSystemWatcher1.InternalBufferSize = 1024 * 8;
+        }
+        private static void CopyDirectory(string sourceDir, string targetDir)
+        {
+            // フォルダが存在しない場合は作成
+            if (!Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+            }
+
+            // フォルダ内のファイルをコピー
+            foreach (string filePath in Directory.GetFiles(sourceDir))
+            {
+                string fileName = Path.GetFileName(filePath);
+                string destPath = Path.Combine(targetDir, fileName);
+                File.Copy(filePath, destPath, true); // true は上書きを許可
+            }
+
+            // サブフォルダも再帰的にコピー
+            foreach (string subDir in Directory.GetDirectories(sourceDir))
+            {
+                string subDirName = Path.GetFileName(subDir);
+                string destSubDir = Path.Combine(targetDir, subDirName);
+                CopyDirectory(subDir, destSubDir);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "監視するフォルダを選択してください";
+                folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    // 選択されたフォルダのパス
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+
+                    // "C:\" にフォルダ名を付けてコピー
+                    string destinationPath = Path.Combine("C:\\", Path.GetFileName(selectedFolderPath));
+                    CopyDirectory(selectedFolderPath, destinationPath);
+
+                    // パスが選択された場合、そのパスを監視対象として設定する
+                    fileSystemWatcher1.Path = selectedFolderPath;
+
+                    // 監視を実行する
+                    fileSystemWatcher1.EnableRaisingEvents = true;
+                }
+            }
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+
+            // 監視を停止する
+            fileSystemWatcher1.IncludeSubdirectories = false;
+        }
+        private static void fileChanged(object sender, FileSystemEventArgs e)
+        {
+
+            string s = String.Format($"{DateTime.Now:yyyy/MM/dd HH:mm:ss} :");
+
+
+            //変更があったときに結果を表示する
+            switch (e.ChangeType)
+            {
+                case System.IO.WatcherChangeTypes.Changed:
+                    s += $"【{e.ChangeType.ToString()}】{e.FullPath}";
+
+                    break;
+                case System.IO.WatcherChangeTypes.Created:
+                    s += $"【{e.ChangeType.ToString()}】{e.FullPath}";
+
+                    break;
+                case System.IO.WatcherChangeTypes.Deleted:
+                    s += $"【{e.ChangeType.ToString()}】{e.FullPath}";
+                    break;
+            }
+            MessageBox.Show(s);
+        }
+
+        /// <summary>
+        /// ファイル名変更
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void fileRenamed(object sender, RenamedEventArgs e)
+        {
+            string s = String.Format($"{DateTime.Now:yyyy/MM/dd HH:mm:ss} :");
+            s += $"【{e.ChangeType.ToString()}】{e.OldName} → {e.FullPath}";
+            MessageBox.Show(s);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
